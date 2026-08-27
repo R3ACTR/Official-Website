0e420ebdec6311626e5cbe7c9c6ff14b5cdd4a29
SREEHARI <sreehari14shr@gmail.com>
2026-08-27 15:18:34 +0530

    refactor: remove starlight integration and add content collections and dynamic web manifest support

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -44,8 +44,8 @@ export const GET: APIRoute = async () => {
   );
 
   const manifest = {
-    short_name: 'ScrewFast',
-    name: 'ScrewFast',
+    short_name: 'R3ACTR',
+    name: 'R3ACTR',
     icons,
     display: 'minimal-ui',
     id: '/',
